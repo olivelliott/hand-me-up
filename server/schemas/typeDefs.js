7e89d83418b9813bdaf435476dcd6c324be9ee76
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-09-14 11:01:11 -0400

    queries and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,6 @@
 // define every piece of data that the client can expect to work with
 
-// TODO: how to set up the additional donation -- this could be an add-on product that's 'suggested to add to cart' on the checkout page? then, it will easily show up in the order as a product purchased
+// TODO: how to set up the additional donation -- this could be an add-on product that's 'suggested to add to cart' on the checkout page? then, it will easily show up in the order as a product purchased.. maybe MVP has a few charities to choose from this way we can create them as products that can be added to the cart in $1 increments?
 
 const { gql } = require("apollo-server-express");
 
@@ -16,6 +16,7 @@ const typeDefs = gql`
         name: String
         description: String
         image: String
+        quantity: Int
         price: Float
         category: Category
     }
@@ -43,9 +44,22 @@ const typeDefs = gql`
         user: User
     }
 
-    type Query {}
+    type Query {
+        categories: [Category]
+        products(category: ID, name: String): [Product]
+        product(_id: ID!): Product
+        user: User
+        order(_id: ID!): Order
+        checkout(products: [ID]!): Checkout
+    }
 
-    type Mutation {}
+    type Mutation {
+        addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+        addOrder(products: [ID]!): Order
+        updateUser(firstName: String, lastName: String, email: String, password: String): User
+        updateProduct(_id: ID!, quantity: Int!): Product
+        login(email: String!, password: String!): Auth
+    }
 `;
 
 module.exports = typeDefs;
