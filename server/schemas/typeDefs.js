4d1f4b52dd013e58c50867fd6de799d79546520b
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-09-16 13:44:22 -0400

    updated syntax for addProduct mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -87,7 +87,7 @@ const typeDefs = gql`
         updateUser(firstName: String, lastName: String, email: String, password: String): User
         updateProduct(_id: ID!, quantity: Int!): Product
         login(email: String!, password: String!): Auth
-        addProduct($name: String!, $brand: String, $size: String!, $description: String, $image: String, $quantity: Int, $price: Float, $category: String): Product
+        addProduct(name: String!, brand: String, size: String!, description: String, image: String, quantity: Int, price: Float, category: String): Product
     }
 `;
 
