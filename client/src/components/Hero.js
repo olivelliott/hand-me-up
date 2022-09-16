34d5ac464d66d5105c961f0753058f8871fa2915
aMcCode <53410824+aMcCode@users.noreply.github.com>
2022-09-16 07:47:56 -0400

    created hero component, updated app.js, note there is an issue with addProduct typeDef in schema. Commented out so client server would run.

---
+++ client/src/components/Hero.js
@@ -0,0 +1,71 @@
+import React from "react";
+import {
+  chakra,
+  Box,
+  Flex,
+  Badge,
+  SimpleGrid,
+  Image,
+} from "@chakra-ui/react";
+
+export default function App(){
+  return (
+    <SimpleGrid
+      columns={{ base: 1, md: 2 }}
+      spacing={0}
+      _after={{
+        bg: "brand.500",
+        opacity: 0.25,
+        pos: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        zIndex: -1,
+        content: '" "',
+      }}
+    >
+      <Flex
+        direction="column"
+        alignItems="start"
+        justifyContent="center"
+        px={{ base: 4, lg: 20 }}
+        py={24}
+      >
+        <Badge
+          color="white"
+          px={3}
+          py={1}
+          mb={3}
+          variant="solid"
+          colorScheme="brand"
+          rounded="full"
+        >
+          Pre Beta
+        </Badge>
+        <chakra.h1
+          mb={6}
+          fontSize={{ base: "4xl", md: "4xl", lg: "5xl" }}
+          fontWeight="bold"
+          color="brand.600"
+          _dark={{ color: "gray.300" }}
+          lineHeight="shorter"
+        >
+          WILL FIND A GOOD QUOTE AND SOME ICONS, WILL FIND NEW IMAGE.
+        </chakra.h1>
+      </Flex>
+      <Box>
+        <Image
+          src="https://images.unsplash.com/photo-1531548731165-c6ae86ff6491?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80"
+          alt="3 women looking at a laptop"
+          fit="cover"
+          w="full"
+          h={{ base: 64, md: "full" }}
+          bg="gray.100"
+          loading="lazy"
+        />
+      </Box>
+    </SimpleGrid>
+  );
+};
+
