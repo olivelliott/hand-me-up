c1dea7bd1ff51cf7e71f8ca123974ea713b26e61
cardinal <caren@merzhaus.org>
2022-09-16 17:57:14 -0400

    card template added, figuring out mapping through products

--- client/src/pages/AllProducts.js
+++ client/src/pages/AllProducts.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import ProductCard from '../components/ProductCard'
+// import { Grid } from '@chakra-ui/react'
 
-export default function AllProducts () {
+export default function AllProducts() {
   return (
-    <h1>{`map.products((product) => <div>ProductCard</div>)`}</h1>
+    <h1>Currently available:</h1>
+      
+        {map.products((product) => 
+        <ProductItem
+        
+        image={product.image}
+        name={product.name}
+        size={product.size}
+        description={product.description}
+        brand={product.brand}
+        price={product.price}
+        category={product.category}
+        quantity={product.quantity}
+        user={product.user}
+
+          />
+          )}
+      
   )
 }
