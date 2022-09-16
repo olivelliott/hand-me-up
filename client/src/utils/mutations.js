7f6b70e18b44deaac09623b53d6ed4d9dae333f6
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-09-16 12:46:08 -0400

    comments

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,7 @@
 import { gql } from '@apollo/client';
 
+// this must match what was set up on the server
+
 export const LOGIN = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
