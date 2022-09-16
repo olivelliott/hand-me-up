c17c1ab4b0918484a274af5d638f6c2d93854575
Matt McGee <mlmcgeenc@gmail.com>
2022-09-16 11:20:04 -0400

    adjust login form to use email as per mutiation in resolvers

--- client/src/pages/LoginForm.js
+++ client/src/pages/LoginForm.js
@@ -1,8 +1,11 @@
 import React from "react";
 import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
   InputGroup,
   Input,
-//   InputRightElement,
   Button,
   Stack,
 } from "@chakra-ui/react";
@@ -12,28 +15,28 @@ export default function LoginForm() {
     // const handleClick = () => setShow(!show)
   
     return (
-      <InputGroup size='md'>
-        <Stack spacing={3}>
-        <Input htmlSize={4} width='auto' placeholder='Username' />
-        <Input pr='4.5rem' placeholder='Enter password' />
-        {/* <Input pr='4.5rem' type={show ? 'text' : 'password'} placeholder='Enter password' /> */}
-        {/* <InputRightElement width='4.5rem'>
-          <Button h='1.75rem' size='sm' onClick={handleClick}> {show ? 'Hide' : 'Show'}</Button>
-        </InputRightElement> */}
-          <Button
-            display={{ base: 'none', md: 'inline-flex' }}
-            fontSize={'sm'}
-            fontWeight={600}
-            color={'white'}
-            bg={'#0A9396'}
-            href={'#'}
-            _hover={{
-              bg: '#001219',
-            }}
-          >
-            Sign In!
-          </Button>
-        </Stack>
-      </InputGroup>
+      <form>
+        <FormControl>
+          <FormLabel>Email</FormLabel>
+          <Input type='email' />
+
+          <FormLabel>Password</FormLabel>
+          <Input type='password' />
+        </FormControl>
+        <Button
+          display={{ base: 'none', md: 'inline-flex' }}
+          fontSize={'sm'}
+          fontWeight={600}
+          color={'white'}
+          bg={'#0A9396'}
+          my={5}
+          href={'#'}
+          _hover={{
+            bg: '#001219',
+          }}
+        >
+          Sign In!
+        </Button>
+      </form>
     )
   }
