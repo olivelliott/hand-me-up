a765f3ce8b103b896e4d8df99e06681a19e7ecde
cardinal <caren@merzhaus.org>
2022-09-17 18:03:53 -0400

    grid somewhat working

--- client/src/pages/AllProducts.js
+++ client/src/pages/AllProducts.js
@@ -1,12 +1,17 @@
 import {
   Flex,
   Box,
+  Grid,
+  GridItem,
+  Wrap,
+  AspectRatio,
   Image,
   useColorModeValue,
   Icon,
   chakra,
   Tooltip,
 } from '@chakra-ui/react'
+
 import { FiShoppingCart } from 'react-icons/fi'
 import { useQuery } from '@apollo/client'
 import { QUERY_ALL_PRODUCTS } from '../utils/queries'
@@ -101,90 +106,96 @@ function Allproducts() {
 
   return (
     <div>
-      {products.map((product) => (
-        <Flex p={50} w="full" alignItems="center" justifyContent="center">
-          <Box
-            // bg={useColorModeValue('white', 'gray.800')}
-            maxW="sm"
-            borderWidth="1px"
-            rounded="lg"
-            shadow="lg"
-            position="relative"
-          >
-            {product.isNew && (
-              <Circle
-                size="10px"
-                position="absolute"
-                top={2}
-                right={2}
-                bg="red.200"
+      <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+        {products.map((product) => (
+          <Flex p={50} w="full" alignItems="center" justifyContent="center">
+            <Box
+              bg="#94d2bd"
+              maxW="sm"
+              borderWidth="1px"
+              rounded="lg"
+              shadow="lg"
+              position="relative"
+            >
+              {product.isNew && (
+                <Circle
+                  size="10px"
+                  position="absolute"
+                  top={2}
+                  right={2}
+                  bg="red.200"
+                />
+              )}
+
+              <Image
+                src={product.image}
+                alt={`Picture of ${product.name}`}
+                roundedTop="lg"
               />
-            )}
 
-            <Image
-              src={product.image}
-              alt={`Picture of ${product.name}`}
-              roundedTop="lg"
-            />
-
-            <Box p="6">
-              <Box d="flex" alignItems="baseline">
-                {product.isNew && (
-                  <Badge
-                    rounded="full"
-                    px="2"
-                    fontSize="0.8em"
-                    colorScheme="red"
+              <Box p="6">
+                <Box d="flex" alignItems="baseline">
+                  {product.isNew && (
+                    <Badge
+                      rounded="full"
+                      px="2"
+                      fontSize="0.8em"
+                      colorScheme="red"
+                    >
+                      New
+                    </Badge>
+                  )}
+                </Box>
+                <Flex
+                  mt="1"
+                  justifyContent="space-between"
+                  alignContent="center"
+                >
+                  <Box
+                    mr="5"
+                    fontSize="2xl"
+                    fontWeight="semibold"
+                    as="h4"
+                    lineHeight="tight"
+                    isTruncated
                   >
-                    New
-                  </Badge>
-                )}
-              </Box>
-              <Flex mt="1" justifyContent="space-between" alignContent="center">
-                <Box
-                  mr="5"
-                  fontSize="2xl"
-                  fontWeight="semibold"
-                  as="h4"
-                  lineHeight="tight"
-                  isTruncated
-                >
-                  {product.name}
-                </Box>
-                <Tooltip
-                  label="Add to cart"
-                  bg="white"
-                  placement={'top'}
-                  color={'gray.800'}
-                  fontSize={'1.2em'}
-                >
-                  <chakra.a href={'#'} display={'flex'}>
-                    <Icon
-                      as={FiShoppingCart}
-                      h={7}
-                      w={7}
-                      alignSelf={'center'}
-                      onClick={handleAddToCart}
-                    />
-                  </chakra.a>
-                </Tooltip>
-              </Flex>
-
-              <Flex justifyContent="space-between" alignContent="center">
-                <Box
-                  fontSize="2xl"
-                  // color={useColorModeValue('gray.800', 'white')}
-                >
-                  <Box as="span" color={'gray.600'} fontSize="lg">
-                    £
+                    {product.name}
                   </Box>
-                  {product.price.toFixed(2)}
-                </Box>
-              </Flex>
+                  <Tooltip
+                    label="Add to cart"
+                    bg="white"
+                    placement={'top'}
+                    color={'gray.800'}
+                    fontSize={'1.2em'}
+                  >
+                    <chakra.a href={'#'} display={'flex'}>
+                      <Icon
+                        as={FiShoppingCart}
+                        h={7}
+                        w={7}
+                        alignSelf={'center'}
+                        onClick={handleAddToCart}
+                      />
+                    </chakra.a>
+                  </Tooltip>
+                </Flex>
+
+                <Flex justifyContent="space-between" alignContent="center">
+                  <Box
+                    fontSize="2xl"
+                    // color={useColorModeValue('gray.800', 'white')}
+                  >
+                    <Box as="span" color={'gray.600'} fontSize="lg">
+                      £
+                    </Box>
+                    {product.price.toFixed(2)}
+                  </Box>
+                </Flex>
+              </Box>
             </Box>
-          </Box>
-        </Flex>
-      ))}
+          </Flex>
+        ))}
+      </Grid>
     </div>
   )
 }
