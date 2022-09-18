bc72a741769fc106d6cb0b3967db5865adefb3da
Matt McGee <mlmcgeenc@gmail.com>
2022-09-18 16:45:18 -0400

    only show submit product form if the user is logged in

--- client/src/pages/SubmitProduct.js
+++ client/src/pages/SubmitProduct.js
@@ -39,6 +39,8 @@ export default function SubmitProduct () {
     category: ''
   })
 
+  const loggedIn = Auth.loggedIn()
+
   const [addProduct, { error }] = useMutation(ADD_PRODUCT)
 
   const handleChange = (e) => {
@@ -48,131 +50,141 @@ export default function SubmitProduct () {
       console.log(formState)
   }
 
-  const handleCountChange = (e) => {
-    setFormState({ ...formState, quantity: e })
+  const handleQuantityChange = (e) => {
+    const count = parseInt(e)
+    setFormState({ ...formState, quantity: count })
     console.log(formState)
+  }
+
+  const handlePriceChange = (e) => {
+    const price = parseFloat(e.target)
+    setFormState({ ...formState, price: price })
   }
 
   const handleFormSubmit = async (e) => {
     e.preventDefault()
 
     try {
-      const { data } = await addProduct({
+      await addProduct({
         variables: { ...formState }
       })
-      console.log(data)
-      Auth.login(data.user.token)
     } catch (err) {
       console.error(err)
     }
   }
 
   return (
-    <form>
-      <Text fontSize='4xl' as='b'>Submit a new item for sale!</Text>
-      <FormControl mt='5' mb='2'>
-        <FormLabel>Item Name</FormLabel>
-        <Input
-          type='text'
-          name='name'
-          onChange={handleChange}
-          placeholder='Shirt'/>
-      </FormControl>
+    <div>
+      {loggedIn ? (
+        <form onSubmit={handleFormSubmit}>
+          <Text fontSize='4xl' as='b'>Submit a new item for sale!</Text>
+          <FormControl mt='5' mb='2'>
+            <FormLabel>Item Name</FormLabel>
+            <Input
+              type='text'
+              name='name'
+              onChange={handleChange}
+              placeholder='Shirt' />
+          </FormControl>
 
-      <FormControl mb='2'>
-        <FormLabel>Brand</FormLabel>
-        <Input
-          type='text'
-          name='brand'
-          onChange={handleChange}
-          placeholder='Acme'/>
-      </FormControl>
+          <FormControl mb='2'>
+            <FormLabel>Brand</FormLabel>
+            <Input
+              type='text'
+              name='brand'
+              onChange={handleChange}
+              placeholder='Acme' />
+          </FormControl>
 
-      <FormControl mb='2'>
-        <FormLabel>Size</FormLabel>
-        <Select 
-          placeholder='choose a size'
-          name='size'
-          onChange={handleChange}>
-          <option value='xs'>XS</option>
-          <option value='s'>S</option>
-          <option value='m'>M</option>
-          <option value='lg'>LG</option>
-          <option value='xl'>XL</option>
-          <option value='xxl'>XXL</option>
-          <option value='xxxl'>XXXL</option>
-        </Select>
-      </FormControl>
+          <FormControl mb='2'>
+            <FormLabel>Size</FormLabel>
+            <Select
+              placeholder='choose a size'
+              name='size'
+              onChange={handleChange}>
+              <option value='xs'>XS</option>
+              <option value='s'>S</option>
+              <option value='m'>M</option>
+              <option value='lg'>LG</option>
+              <option value='xl'>XL</option>
+              <option value='xxl'>XXL</option>
+              <option value='xxxl'>XXXL</option>
+            </Select>
+          </FormControl>
 
-      <FormControl mb='2'>
-        <FormLabel>Description</FormLabel>
-        <Textarea
-          name="description"
-          onChange={handleChange}
-          placeholder="Please describe the item, it's features, and it's condition"></Textarea>
-      </FormControl>
+          <FormControl mb='2'>
+            <FormLabel>Description</FormLabel>
+            <Textarea
+              name="description"
+              onChange={handleChange}
+              placeholder="Please describe the item, it's features, and it's condition"></Textarea>
+          </FormControl>
 
-      <FormControl mb='2'>
-        <Box>
-          <Flex >
-            <Box width="79%">
-              <FormLabel>Price</FormLabel>
-              <InputGroup>
-                <InputLeftElement
-                  pointerEvents='none'
-                  color='gray.300'
-                  fontSize='1.2em'
-                  children='$'
-                />
-                <Input
-                  placeholder='Enter amount'
-                  name='price'
-                  onChange={handleChange}/>
-              </InputGroup>
+          <FormControl mb='2'>
+            <Box>
+              <Flex >
+                <Box width="79%">
+                  <FormLabel>Price</FormLabel>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents='none'
+                      color='gray.300'
+                      fontSize='1.2em'
+                      children='$'
+                    />
+                    <Input
+                      placeholder='Enter amount'
+                      name='price'
+                      onChange={handlePriceChange} />
+                  </InputGroup>
+                </Box>
+                <Spacer />
+                <Box width="20%">
+                  <FormLabel>Quantity</FormLabel>
+                  <NumberInput
+                    name='quantity'
+                    onChange={handleQuantityChange}
+                    defaultValue={0}
+                    min={1}>
+                    <NumberInputField />
+                    <NumberInputStepper>
+                      <NumberIncrementStepper />
+                      <NumberDecrementStepper />
+                    </NumberInputStepper>
+                  </NumberInput>
+                </Box>
+              </Flex>
             </Box>
-            <Spacer />
-            <Box width="20%">
-              <FormLabel>Quantity</FormLabel>
-              <NumberInput
-                name='quantity'
-                onChange={handleCountChange}
-                defaultValue={0}
-                min={1}>
-                <NumberInputField />
-                <NumberInputStepper>
-                  <NumberIncrementStepper />
-                  <NumberDecrementStepper />
-                </NumberInputStepper>
-              </NumberInput>
-            </Box>
-          </Flex>
-        </Box>
-      </FormControl>
+          </FormControl>
 
-      <FormControl mb='2'>
-        <FormLabel>Category</FormLabel>
-        <Select
-          placeholder='choose a category'
-          name='category'
-          onChange={handleChange}>
-          <option value='men'>Men's</option>
-          <option value='women'>Women's</option>
-        </Select>
-      </FormControl>
+          <FormControl mb='2'>
+            <FormLabel>Category</FormLabel>
+            <Select
+              placeholder='choose a category'
+              name='category'
+              onChange={handleChange}>
+              <option value='men'>Men's</option>
+              <option value='women'>Women's</option>
+            </Select>
+          </FormControl>
 
-      <Button
-        type='submit'
-        display={{ base: 'none', md: 'inline-flex' }}
-        fontSize={'sm'}
-        fontWeight={600}
-        color={'white'}
-        bg={'#0A9396'}
-        my={5}
-        href={'#'}
-        _hover={{
-          bg: '#001219',
-        }}
-      >Submit this item</Button>
-    </form>
+          <Button
+            type='submit'
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'sm'}
+            fontWeight={600}
+            color={'white'}
+            bg={'#0A9396'}
+            my={5}
+            href={'#'}
+            _hover={{
+              bg: '#001219',
+            }}
+          >Submit this item</Button>
+        </form>
+      ) : (
+        <h1>You must be logged in to submit new items</h1>
+      )}
+    </div>
   )
 }
