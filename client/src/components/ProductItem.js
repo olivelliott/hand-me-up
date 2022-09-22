5dc578ad136b4131062b64644b07c84811463464
Olive <olivelliott48@gmail.com>
2022-09-21 21:14:21 -0400

    Update cart back to calculate totals and play with global state

--- client/src/components/ProductItem.js
+++ client/src/components/ProductItem.js
@@ -179,16 +179,3 @@ function ProductItem(item) {
 
 export default ProductItem;
 
-{/* <div className="card px-1 py-1">
-<Link to={`/products/${_id}`}>
-  <img alt={name} src={`/images/${image}`} />
-  <p>{name}</p>
-</Link>
-<div>
-  <div>
-    {quantity} {pluralize("item", quantity)} in stock
-  </div>
-  <span>${price}</span>
-</div>
-<button onClick={addToCart}>Add to cart</button>
-</div> */}
--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -285,6 +285,63 @@ export default function Cart() {
 
   const [state, dispatch] = useStoreContext();
 
+  let initialCartCount = sessionStorage.length > 1 ? sessionStorage.length - 1 : 1
+  let initialSubTotal = initialCartCount * 2.99
+  let initialTax = initialSubTotal * .0475
+  let initialTotal = initialSubTotal + initialTax
+
+  const [itmCount, setCount] = useState([{itmId: 0, Count: state.length, Price: 2.99 * state.length}])
+  const [totalCount, setTotalCount] = useState(initialCartCount)
+  const [subTotal, setSubTotal] = useState(initialSubTotal)
+  const [shipAndTax, setTax] = useState(initialTax)
+  const [totalCost, setTotalCost] = useState(initialTotal)
+
+  useEffect(() => {setTotalItemCount()}, [itmCount])
+  useEffect(() => {set_Sub_Total()}, [totalCount])
+  useEffect(() => {set_Tax()}, [subTotal])
+  useEffect(() => {set_Total_Cost()}, [shipAndTax])
+
+
+  const setTotalItemCount = () => {
+    let total = 0;
+    for(let i=0; i < itmCount.length; i++) {
+      total += Number(itmCount[i].Count)
+    }
+    if(total > 0)
+      setTotalCount(total)
+    else
+      setTotalCount(initialCartCount)
+  }
+
+  const set_Sub_Total = () => {
+    let total = 0;
+    for(let i=0; i < itmCount.length; i++) {
+      total += Number(itmCount[i].Price)
+    }
+    if(total > 0)
+      setSubTotal(total)
+    else
+      setSubTotal(initialSubTotal)
+  }
+
+  const set_Tax = () => {
+    let total = 0
+    total = total + (subTotal * .0475)
+    if(total > 0)
+      setTax(total.toFixed(2))
+    else
+      setTax(initialTax)
+  }
+
+  const set_Total_Cost = () => {
+    let total = 0.0
+    total = parseFloat(subTotal) + parseFloat(shipAndTax)
+    if(total > 0)
+      setTotalCost(total.toFixed(2))
+    else
+      setTotalCost(initialTotal)
+  }
+
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise("cart", "get");
@@ -296,17 +353,14 @@ export default function Cart() {
     }
   }, [state.cart.length, dispatch]);
 
-  function toggleCart() {
-    dispatch({ type: TOGGLE_CART });
-  }
+  // function calculateTotal() {
+  //   let sum = 0;
+  //   state.cart.forEach((item) => {
+  //     sum += item.price * item.purchaseQuantity;
+  //   });
+  //   return sum.toFixed(2);
+  // }
 
-  function calculateTotal() {
-    let sum = 0;
-    state.cart.forEach((item) => {
-      sum += item.price * item.purchaseQuantity;
-    });
-    return sum.toFixed(2);
-  }
 
   // function submitCheckout() {
   //   const productIds = [];
@@ -341,7 +395,7 @@ export default function Cart() {
   // }
 
   return(
-  <Container ml={0}>
+  <Container ml={0} fontFamily='body'>
   <Flex d='columns'>
   <Box align='left' key={'main-box'}
     maxW={{ base: '3xl', lg: '7xl' }}
@@ -360,39 +414,29 @@ export default function Cart() {
         </Heading>
           <List key='product-list'>
           {state.cart.map((item) => (
-      //     <Box key={'purchase_'+item._id} className={'purchase_'+item._id}>
-      //     <Flex>
-      //       <Box mb='5' mr='5' minH='200px' maxH='200px' minW='200px' maxW='200px'>
-      //         <Image boxSize='200px' objectFit='cover' src={`images/${item.image}`} alt="product" />
-      //       </Box>
-      //       <Box>
-      //         <ListItem key={'brand_'+item._id} mr='5' maxH='20px' minW='200px' maxW='200px'>{item.brand}</ListItem>
-      //         <ListItem key={'name_'+item._id} mb='2' mr='5' minH='30px' maxH='30px' minW='200px' maxW='200px'><b>{item.name}</b></ListItem>
-      //         <ListItem key={'size_'+item._id} mb='5' mr='5' maxH='10px' minW='200px' maxW='200px'>{item.size}</ListItem>
-      //         <ListItem key={'desc_'+item._id} mr='5' minH='175px' maxH='200px' minW='200px' maxW='200px'>{item.description}</ListItem>
+          <Box key={'purchase_'+item._id} className={'purchase_'+item._id}>
+          <Flex>
+            <Box mb='5' mr='5' minH='200px' maxH='200px' minW='200px' maxW='200px'>
+              <Image boxSize='200px' objectFit='cover' src={`images/${item.image}`} alt="product" />
+            </Box>
+            <Box>
+              <ListItem key={'brand_'+item._id} mr='5' maxH='20px' minW='200px' maxW='200px'>{item.brand}</ListItem>
+              <ListItem key={'name_'+item._id} mb='2' mr='5' minH='30px' maxH='30px' minW='200px' maxW='200px'><b>{item.name}</b></ListItem>
+              <ListItem key={'size_'+item._id} mb='5' mr='5' maxH='10px' minW='200px' maxW='200px'>{item.size}</ListItem>
+              <ListItem key={'desc_'+item._id} mr='5' minH='175px' maxH='200px' minW='200px' maxW='200px'>{item.description}</ListItem>
 
-      //         </Box>
-      //         <Box  minH='200px' maxH='200px' minW='125px' maxW='200px'>
-      //         <ListItem key={'price_'+item._id} mb='10' mr='5' maxH='10px' minW='200px' maxW='200px'><b>${item.price}</b></ListItem>
-      //         <ListItem key={'select_'+item._id}>
-      //         <Select maxW='75px' border='2px' borderColor='black' aria-label="Select quantity"
-      //         defaultValue="1"
-      //          onChange={(e) => handleUpdateSummary(e, item._id, e.target.value, item.price)}
-
-      //          className='add-item-input'>
-      //           <option value="1">1</option>
-      //           <option value="2">2</option>
-      //           <option value="3">3</option>
-      //           <option value="4">4</option>
-      //         </Select>
-      //         <Button key={'btn_rmv'+item._id}bg='red' color='white' size="sm" fontSize="sm" _hover={{ bg: 'brick_red'}} onClick={() => rmvFromCart(item)}>
-      //   Remove
-      // </Button>
-      //         </ListItem>
-      //         </Box>
-      //     </Flex>
-      //     </Box>
-        <CartItem key={item._id} item={item} />
+              </Box>
+              <Box  minH='200px' maxH='200px' minW='125px' maxW='200px'>
+              <ListItem key={'price_'+item._id} mb='10' mr='5' maxH='10px' minW='200px' maxW='200px'><b>${item.price}</b></ListItem>
+              <ListItem key={'select_'+item._id}>
+              <Button key={'btn_rmv'+item._id}bg='red' color='white' size="sm" fontSize="sm" _hover={{ bg: 'brick_red'}}>
+                Remove
+              </Button>
+              </ListItem>
+              </Box>
+          </Flex>
+          </Box>
+        // <CartItem key={item._id} item={item} />
           ))
           }
           </List>
@@ -403,7 +447,7 @@ export default function Cart() {
 <Stack spacing="8" borderWidth="1px" rounded="lg" padding="8" width='300px'>
       <Heading size="md">Order Summary</Heading>
       <Stack spacing="6">
-      {/* <InputGroup>
+      <InputGroup>
           <InputLeftAddon children="ItemCount" />
           <Input placeholder={totalCount} />
         </InputGroup>
@@ -414,10 +458,10 @@ export default function Cart() {
         <InputGroup>
           <InputLeftAddon children="Shipping + Tax" />
           <Input placeholder={shipAndTax}/>
-        </InputGroup> */}
+        </InputGroup>
         <InputGroup>
           <InputLeftAddon children="Total" />
-          <Input placeholder={calculateTotal} />
+          <Input placeholder={totalCost} />
         </InputGroup>
       <Button bg='red' color='white' size="lg" fontSize="md" _hover={{ bg: 'brick_red'}}>
         Checkout
